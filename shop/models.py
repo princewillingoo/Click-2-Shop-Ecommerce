1694128fb7ff6fafa70811c6a3a625bc25de5ea1
princewilling <princewillingoo@gmail.com>
2023-01-29 17:03:14 +0100

    Building catalog views

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -15,7 +16,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", kwargs={"slug": self.slug})
     
+    
+
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
@@ -37,3 +42,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
+     
