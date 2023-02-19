e676e36d1160e23300ea206b704398d7ed566116
princewilling <princewillingoo@gmail.com>
2023-02-19 22:45:10 +0100

    Applying coupons to orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
+from coupons.models import Coupon
 from django.conf import settings
 from django.db import models
 from shop.models import Product
@@ -13,6 +16,9 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paystack_payment_ref = models.CharField(max_length=15, blank=True)
     paid = models.BooleanField(default=False)
+    coupon = models.ForeignKey(Coupon,
+    related_name='orders', null=True, blank=True, on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
     
     class Meta:
         ordering = ['-created']
@@ -23,8 +29,18 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
     
-    def get_total_cost(self):
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+    
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
+    
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
     
     def get_paystack_ref_url(self):
         if not self.paystack_payment_ref:
